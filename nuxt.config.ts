af3243be1ae70e422c4ca23841a2c297dc9fead7
Luis Rivera <rivera.luisjose@gmail.com>
2024-06-12 07:54:08 -0400

    more changes to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: false,
+
+  routeRules: {
+    "/api/**": { cors: true },
+    "/_ipx/**": { prerender: true },
+  },
+
+  nitro: {
+    // preset: "firebase",
+    // firebase: {
+    //   nodeVersion: "18",
+    //   gen: 1,
+    //   region: "us-central1",
+    // },
+  },
+
+  devServer: {
+    // https: {
+    //   // key: "certs/localhost-key.pem",
+    //   // cert: "certs/localhost.pem",
+    //   key: "certs/192.168.50.10-key.pem",
+    //   cert: "certs/192.168.50.10.pem",
+    // },
+    host: "0.0.0.0", // default: localhost,
+    // port: 5000, // default: 3000
+  },
 });
